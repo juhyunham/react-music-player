78798b24c8137f93bd3de557339657f574a7a300
hamju <judev0227@gmail.com>
2022-07-26 11:28:58 +0900

    앱tsx 타이틀, 정보, 이미지 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Title from "./components/Title/Title";
+import DetailImage from "./components/DetailImage/DetailImage";
+import Info from "./components/Info/Info";
 import Controls from "./components/Controls/Controls";
 import "./App.scss";
 
 function App() {
   return (
     <div className="music_container">
+      <Title />
+      <DetailImage />
+      <Info />
       <Controls />
     </div>
   );
