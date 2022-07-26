89ad3b3d4761c114f4b4be4869ab5a345cd07d43
hamju <judev0227@gmail.com>
2022-07-26 11:11:17 +0900

    control panel update

---
+++ src/components/Controls/Controls.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Controls.scss";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import PauseIcon from "@mui/icons-material/Pause";
+import SkipPrevious from "@mui/icons-material/SkipPrevious";
+import PlayArrow from "@mui/icons-material/PlayArrow";
+import SkipNext from "@mui/icons-material/SkipNext";
+import QueueMusic from "@mui/icons-material/QueueMusic";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+
+export default function Controls() {
+  return (
+    <div className="control_area">
+      <QueueMusic sx={{ fontSize: 30, cursor: "pointer" }} />
+      <RepeatIcon sx={{ fontSize: 30, cursor: "pointer" }} />
+      <SkipPrevious sx={{ fontSize: 30, cursor: "pointer" }} />
+
+      {true ? (
+        <PauseIcon sx={{ fontSize: 30, cursor: "pointer" }} />
+      ) : (
+        <PlayArrow className="play" sx={{ fontSize: 30, cursor: "pointer" }} />
+      )}
+
+      <SkipNext sx={{ fontSize: 30, cursor: "pointer" }} />
+
+      <div className="volume_container">
+        <VolumeUpIcon sx={{ fontSize: 20 }} />
+        <input type="range" style={{ cursor: "pointer" }} defaultValue={1} min="0" max="1" step="0.1" />
+      </div>
+    </div>
+  );
+}
