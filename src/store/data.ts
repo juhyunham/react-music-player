21dd245bf372ab2831e14ae1856ba10d056fb256
hamju <judev0227@gmail.com>
2022-07-26 20:27:30 +0900

    mp3 file-loader, url-loader error로인한 주석

--- src/store/data.ts
+++ src/store/data.ts
@@ -4,46 +4,46 @@ import img3 from "../images/image3.jpg";
 import img4 from "../images/image4.jpg";
 import img5 from "../images/image5.jpg";
 
-import music1 from "../music/music1.mp3";
-import music2 from "../music/music2.mp3";
-import music3 from "../music/music3.mp3";
-import music4 from "../music/music4.mp3";
-import music5 from "../music/music5.mp3";
+// import music1 from "../music/music1.mp3";
+// import music2 from "../music/music2.mp3";
+// import music3 from "../music/music3.mp3";
+// import music4 from "../music/music4.mp3";
+// import music5 from "../music/music5.mp3";
 
 const MusicList = [
   {
     name: "노래노래1",
     artist: "아티스트1",
     img: img1,
-    src: music1,
+    // src: music1,
     id: 1,
   },
   {
     name: "노래2",
     artist: "아티스트2",
     img: img2,
-    src: music2,
+    // src: music2,
     id: 2,
   },
   {
     name: "노래3",
     artist: "아티스트23",
     img: img3,
-    src: music3,
+    // src: music3,
     id: 3,
   },
   {
     name: "노래4",
     artist: "아티스트44",
     img: img4,
-    src: music4,
+    // src: music4,
     id: 4,
   },
   {
     name: "노래5",
     artist: "아티스트55",
     img: img5,
-    src: music5,
+    // src: music5,
     id: 5,
   },
 ];
