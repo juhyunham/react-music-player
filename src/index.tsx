794de0a27df9e5f2723ece42777441ca04fc9909
hamju <judev0227@gmail.com>
2022-08-01 19:05:37 +0900

    typescript error 해결

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import musicPlayerReducer from "./store/musicPlayerReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(musicPlayerReducer, composeWithDevTools);
+const store = configureStore({
+  reducer: musicPlayerReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -19,7 +21,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//web vital
 reportWebVitals();
